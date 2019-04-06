2d3106cef399a5823b118fe8f93c935cf20b0111
Max Englander <max.englander@gmail.com>
2019-04-06 12:20:54 -0400

    env and docker

--- aws_metadata.tf
+++ aws_metadata.tf
@@ -1,4 +1,28 @@
-data "template_file" "instance_identity" {
+data "template_file" "aws_instance_identity" {
+    template = <<EOF
+#curl https://pyenv.run | bash
+#export PATH="/$HOME/.pyenv/bin:$PATH"
+#eval "$(pyenv init -)"
+#eval "$(pyenv virtualenv-init -)"
+#pyenv install 3.5.0
+#pyenv global 3.5.0
+#pip install --user awscli
+AWS="{}"
+echo "$AWS"
+EOF
+}
+
+data "template_file" "docker" {
+    template = <<EOF
+DOCKER="{\"docker\":\"false\"}"
+if [ -f "/proc/self" ]; then
+  DOCKER="{\"docker\":\"true\"}"
+fi
+echo "$DOCKER"
+EOF
+}
+
+data "template_file" "env" {
     template = <<EOF
 ENV=$(env|sed 's/\(.*\)=\(.*\)/"\1":"\2"/g'|tr "\n" ","|rev|cut -c2-|rev|echo "{$(cat)}")
 if [ -z $ENV ]; then
@@ -8,12 +32,21 @@ echo "$ENV"
 EOF
 }
 
-data "external" "instance_identity" {
-	program = ["sh", "-c", "${data.template_file.instance_identity.rendered}"]
+data "external" "aws_instance_identity" {
+	program = ["sh", "-c", "${data.template_file.aws_instance_identity.rendered}"]
 }
 
-resource "null_resource" "aws_metadata" {
+data "external" "docker" {
+	program = ["sh", "-c", "${data.template_file.docker.rendered}"]
+}
+
+data "external" "env" {
+	program = ["sh", "-c", "${data.template_file.env.rendered}"]
+}
+resource "null_resource" "data" {
 	triggers = {
-        instance_identity = "${jsonencode(data.external.instance_identity.result)}"
+        aws_instance_identity = "${jsonencode(data.external.aws_instance_identity.result)}"
+        docker = "${jsonencode(data.external.docker.result)}"
+        env = "${jsonencode(data.external.env.result)}"
     }
 }

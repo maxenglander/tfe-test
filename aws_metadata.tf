0d665d7849c5b62b41d0ad3b35d01e1e2c24abb2
Max Englander <max.englander@gmail.com>
2019-04-06 11:27:21 -0400

    no silent curl

--- aws_metadata.tf
+++ aws_metadata.tf
@@ -1,6 +1,6 @@
 data "template_file" "instance_identity" {
     template = <<EOF
-curl -s http://metadata.google.internal/computeMetadata/v1/instance/zone
+curl http://metadata.google.internal/computeMetadata/v1/instance/zone
 EOF
 }
 
